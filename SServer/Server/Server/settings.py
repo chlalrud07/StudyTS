8942ca7089845143330edc09773ee30834fddd1c
Yu Jeonghoon <moning02004@naver.com>
2019-01-26 17:12:38 +0900

    re:

--- SServer/Server/Server/settings.py
+++ SServer/Server/Server/settings.py
@@ -57,8 +57,8 @@ ROOT_URLCONF = 'Server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': ['templates'],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
