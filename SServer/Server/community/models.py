c574ca86c5027a724b8d7704ed7ee0ce411634c8
Yu Jeonghoon <moning02004@naver.com>
2019-01-26 17:38:28 +0900

    community : file download complete

--- SServer/Server/community/models.py
+++ SServer/Server/community/models.py
@@ -1,19 +1,17 @@
 import os
-
 from django.contrib.auth.models import User
 from django.db import models
-from datetime import datetime
 
 
 def file_path(instance, filename):
-    return '{0}/POST/{1}'.format(instance.post.author.username, filename)
+    return '{0}/POST/{1}'.format(instance.author.id, filename)
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField(max_length=100, null=False)
     content = models.TextField(max_length=500, null=True)
-    created_at = models.DateTimeField(default=datetime.today())
+    created_at = models.DateTimeField(auto_now_add=True)
     attached = models.FileField(upload_to=file_path, null=True)
 
     def delete(self, *args, **kwargs):
